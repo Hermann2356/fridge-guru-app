d129092d503f95232df7910f28fb0c655a3b3d39
Hermann Sterling <hermannsterling@gmail.com>
2020-11-11 15:49:14 -0500

    Add Ingredient Controller. Closes #15

--- api/test/models.test.js
+++ api/test/models.test.js
@@ -3,7 +3,12 @@ const { Ingredient, Category, User, Fridge } = require('../models');
 const seed = require('../../seed');
 
 
-const { getAllIngredients } = require('./models');
+const {
+    getAllIngredients,
+    getAllCategories,
+    getAllUsers,
+    getAllUserIngredients
+} = require('./models');
 
 
 
@@ -16,6 +21,20 @@ describe('1) Model Usage', () => {
     test('get all ingredient', async () => {
         expect(await getAllIngredients()).toEqual(14);
 
-    })
+    });
 
-})
+    test('get all category', async () => {
+        expect(await getAllCategories()).toEqual(3);
+
+    });
+
+    test('get all user', async () => {
+        expect(await getAllUsers()).toEqual(1);
+    });
+
+    test('get all user (Hermann Sterling) ingredients', async () => {
+        expect(await  getAllUserIngredients()).toEqual(["apple","orange","grape","peaches","asparagus","beans","cabbage","turkey","goose","banana"]);
+    })
+});
+
+
