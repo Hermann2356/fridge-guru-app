674308804c515c5a48601a10ea75273111490788
Hermann Sterling <hermannsterling@gmail.com>
2020-11-18 00:59:24 -0500

    Implement recipe controller CRUD methods.

--- api/controllers/recipe.js
+++ api/controllers/recipe.js
@@ -5,16 +5,34 @@ const passport = require('../middlewares/authentication');
 const { Recipe } = db;
 
 router.get('/', (req, res) => {
-
-
+    let user = req.userId;
+    Recipe.findAll({where: {userId: user}})
+        .then(recipes => res.json(recipes));
 });
 
 router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    Recipe.findByPk(id)
+        .then(recipe => {
+            if (!recipe) {
+                return res.sendStatus(404);
+            }
 
+            res.json(recipe);
+        });
 });
 
 router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Recipe.findByPk(id)
+        .then(recipe => {
+            if (!recipe) {
+                return res.sendStatus(404);
+            }
 
+            recipe.destroy();
+            res.sendStatus(204);
+        });
 });
 
 module.exports = router;
