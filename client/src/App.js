6e3c222df64ae2df48d8ac340392c1d94cf0c0fb
Hermann Sterling <hermannsterling@gmail.com>
2020-11-19 19:34:29 -0500

    fixed merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,7 @@ import {
     Link,
     NavLink
 } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
-import PostsListPage from './pages/PostsListPage';
-import PostFormPage from './pages/PostFormPage';
-import AboutUsPage from './pages/AboutUsPage';
-import LoginPage from './pages/LoginPage';
-import PrivateRoute from './components/PrivateRoute';
-import AuthButton from './components/AuthButton';
 import './App.css';
-import CookingPage from "./pages/CookingPage";
 import Home from "./pages/Home";
 
 
@@ -34,7 +26,6 @@ function Navigation(props) {
                     </NavLink>
                 </li>
             </ul>
-            <AuthButton/>
         </nav>
     );
 }
@@ -46,11 +37,6 @@ class App extends React.Component {
             <Router>
                 <div>
                     <Switch>
-                        <Route path="/cooking" component={CookingPage}/>
-                        <Route path="/ingredients" component={IndexPage}/>
-                        <Route path="/login" component={LoginPage}/>
-                        <PrivateRoute path="/posts/new" component={PostFormPage}/>
-                        <Route path="/about-us" component={AboutUsPage}/>
                         <Route path="/" component={ Home }/>
                     </Switch>
                 </div>
