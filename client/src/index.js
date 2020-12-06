53320b9bdce8c955b9dcb325625ad2387574b866
mar-jac <mar.jacq0296@gmail.com>
2020-12-06 11:51:27 -0500

    Added styles for index and additional icons.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-// Import Libraries
 import React from "react";
 import ReactDOM from "react-dom";
-
-// Import Styles
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-// Import Components
 import App from "./App";
 
 ReactDOM.render(
