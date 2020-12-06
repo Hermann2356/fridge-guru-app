d8bfbd2f54b776e24594ba5262c91a77a9c7fa4b
mar-jac <mar.jacq0296@gmail.com>
2020-12-06 11:52:37 -0500

    Added in authenicaiton for CookingPage, RecipePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
-// Import Libraries
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// Import Styles
-import "./App.css";
-
-// Import Pages
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink
+} from 'react-router-dom';
+import './App.css';
 import Home from "./pages/Home";
+import CookingPage from "./pages/CookingPage";
+import AuthenticationPage from "./pages/AuthenticationPage"
 import Profile from "./pages/Profile";
+import RecipePage from "./pages/RecipePage";
 
-// Import Component
-import Navbar from "./components/Navbar";
-
-// Render Method
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/login" component={AuthenticationPage}/>
+                        <Route path="/recipe/cooking" component={CookingPage} />
+                        <Route path="/recipe" component={RecipePage} />
+                        <Route path="/profile" component={Profile} />
+                        <Route path="/" component={Home} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 
 export default App;
