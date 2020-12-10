8de949625db91d4e619ffe8aefa8deb7eeb604e6
Hermann Sterling <hermannsterling@gmail.com>
2020-12-10 16:28:52 -0500

    Add Right bar panel

--- client/src/components/Rightbar.js
+++ client/src/components/Rightbar.js
@@ -4,13 +4,31 @@ import { Input } from "reactstrap";
 import '../components_stylesheets/Rightbar.css';
 
 class Rightbar extends React.Component {
+
+    state = {
+        showNotification: false,
+        iconBorder: "none",
+    }
+
+    enableNotificationPanel = () => {
+        this.setState({
+            showNotification: !this.state.showNotification,
+            iconBorder: this.state.iconBorder === "solid"?"none":"solid"
+        });
+    }
   render() {
     return (
       <div className="right__bar_container">
         <div className="clearfix"></div>
-        <div className="d-flex justify-content-end mt-2 ">
-          <BsBell className="notification__button" />
+        <div className="d-flex justify-content-end mt-2 row col-12 ">
+          <BsBell style={{borderStyle: this.state.iconBorder}} onClick={this.enableNotificationPanel} className="notification__button" />
         </div>
+          {this.state.showNotification?
+              <div className="row col-12 right-notification-panel">
+                <p>No Notifications</p>
+              </div> : ""
+          }
+
           {/*<NotifyMe*/}
           {/*    data=""*/}
           {/*    storageKey='notific_key'*/}
