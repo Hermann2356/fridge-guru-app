79000664f731cfa82f92399352b578106bf85bad
Hermann Sterling <hermannsterling@gmail.com>
2020-12-20 16:25:54 -0500

    Update README.md

--- api/controllers/appConfig.js
+++ api/controllers/appConfig.js
@@ -4,7 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.json({
     title: 'FridgeGuru Web Application',
-    description: 'A short description about this app',
+    description: 'FridgeGuru: is a web application that allows users to keep track of there ingredients and their' +
+        ' expiration dates , while also allowing users to search various recipes that can utilize their ingredients.',
   });
 });
 
